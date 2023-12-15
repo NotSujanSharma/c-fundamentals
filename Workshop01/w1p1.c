84b2fe4a64e1eeef444630ed96f56a47cdb90849
NotSujanSharma <sujan@brokenai.ca>
2023-12-15 12:19:20 -0500

    added Workshop 1

---
+++ Workshop01/w1p1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+	printf("Workshop 1 Part-1 \n=================\n");
+	printf("\nI'm displaying this using the 'printf' stdio\n(standard input output) library function!\n");
+	printf("\nDear teacher,\n\n I promise I will work hard from this day onward.\n I acknowledge that practice is extremely important,\n so I will do all workshops, quizzes, and assignments.\n");
+	printf("\nSincerely,\n\n -------- \nStudent ID# ---------");
+	
+	return 0;
+}
---
+++ Workshop01/w1p2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void) {
+	printf("Workshop 1 Part-2\n================\n");
+	printf("Using tab specifiers...");
+	printf("\tTab-1\tTab-2\tTab-3\n");
+	printf("\t-----\t=====\t-----\n");
+	printf("\\ this is a back-slash character!\n");
+	printf("%% this is a percentage sign character!\n");
+	printf("\" this is a double-quote character!\n");
+	printf("My favourite quotes are:\n");
+	printf("1.\t\"Skill is only developed by hours and hours of work.\"\n\t\t\t\t\t\t-Usain Bolt.\n");
+	printf("2.\t\"It's not about having time. It's about making time.\"\n\t\t\t\t\t\t-unknown\n");
+	printf("3.\t\"All of us do not have equal talent. But, all of us\n\thave equal opportunity to develop out talents.\"\n\t\t\t\t\t\t-A.P.J. Abdul Kalam\n");
+	return 0;
+}
