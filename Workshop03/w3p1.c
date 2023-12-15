2a190fc825d49e2512234f9ed66696dc48067810
NotSujanSharma <sujan@brokenai.ca>
2023-12-15 12:33:10 -0500

    Added Workshop 3 and updated README

---
+++ Workshop03/w3p1.c
@@ -0,0 +1,58 @@
+/*/////////////////////////////////////////////////////////////////////////
+                                                  Workshop - #3 (P1)
+
+                                                  DISCLAIMER
+
+This Work is only for educational purpose, any student who comes across this
+work is not permitted to copy this work. AGAIN Any student who comes across these
+documents is warned NOT TO USE these as their homework as it is plagiarism and
+a clear violation of Seneca's Academic Integrity!
+
+/////////////////////////////////////////////////////////////////////////*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    // You will need this when converting from grams to pounds (lbs)
+    const double testValue = 330.99;
+    int productID1 = 111, productID2 = 222, productID3 = 111;
+    double productPrice1 = 111.49, productPrice2 = 222.99, productPrice3 = 334.49, average;
+    char productTaxed1 = 'Y', productTaxed2 = 'N', productTaxed3 = 'N';
+    printf("Product Information\n");
+    printf("===================\n");
+    printf("Product-1 (ID:%d)\n", productID1);
+    printf("  Taxed: %c\n", productTaxed1);
+    printf("  Price: $%.4lf", productPrice1);
+    printf("\n\n");
+ 
+    printf("Product-2 (ID:%d)\n", productID2);
+    printf("  Taxed: %c\n", productTaxed2);
+    printf("  Price: $%.4lf", productPrice2);
+    printf("\n\n");
+
+    printf("Product-3 (ID:%d)\n", productID3);
+    printf("  Taxed: %c\n", productTaxed3);
+    printf("  Price: $%.4lf", productPrice3);
+    printf("\n\n");
+    average = (productPrice1 + productPrice2 + productPrice3) / 3;
+    printf("The average of all prices is: $%.4lf", average);
+    printf("\n\n");
+    printf("About Relational and Logical Expressions!\n");
+    printf("========================================\n");
+    printf("1. These expressions evaluate to TRUE or FALSE\n");
+    printf("2. FALSE: is always represented by integer value 0\n");
+    printf("3. TRUE : is represented by any integer value other than 0\n\n");
+    printf("Some Data Analysis...\n");
+    printf("=====================\n");
+    printf("1. Is product 1 taxable? -> %d\n\n", (productTaxed1=='Y'));
+    printf("2. Are products 2 and 3 both NOT taxable (N)? -> %d\n\n", (productTaxed2 == 'N' && productTaxed3 == 'N'));
+    printf("3. Is product 2 less than testValue ($%.2lf)? -> %d\n\n", testValue, (productPrice3 < testValue));
+    printf("4. Is the price of product 3 more than both product 1 and 2 combined? -> %d\n\n", (productPrice3 > (productPrice1 + productPrice2)));
+    printf("5. Is the price of product 1 equal to or more than the price difference\n   of product 3 LESS product 2? -> %d (price difference: $%.2lf)\n\n",(productPrice1>=(productPrice3-productPrice2)),(productPrice3-productPrice2));
+    printf("6. Is the price of product 2 equal to or more than the average price? -> %d\n\n",(productPrice2>=average));
+    printf("7. Based on product ID, product 1 is unique -> %d\n\n", !(productID1==productID2||productID1==productID3));
+    printf("8. Based on product ID, product 2 is unique -> %d\n\n", !(productID2 == productID1 || productID2 == productID3));
+    printf("9. Based on product ID, product 3 is unique -> %d\n", !(productID3 == productID1 || productID3 == productID2));
+    return 0;
+}
---
+++ Workshop03/w3p2.c
@@ -0,0 +1,137 @@
+/*/////////////////////////////////////////////////////////////////////////
+                                                  Workshop - #3 (P2)
+
+                                                  DISCLAIMER
+
+This Work is only for educational purpose, any student who comes across this
+work is not permitted to copy this work. AGAIN Any student who comes across these
+documents is warned NOT TO USE these as their homework as it is plagiarism and
+a clear violation of Seneca's Academic Integrity!
+
+/////////////////////////////////////////////////////////////////////////*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+    const double GRAMS_IN_LBS = 453.5924;
+
+    char coffeeType1, coffeeType2, coffeeType3;
+    char cream1, cream2, cream3;
+    char coffeeStrength, likeCream;
+    int coffeeWeight1, coffeeWeight2, coffeeWeight3;
+    int totalServings;
+    
+
+    printf("Take a Break - Coffee Shop");
+    printf("\n==========================");
+    printf("\n\nEnter the coffee product information being sold today...");
+    printf("\n\nCOFFEE-1...");
+    printf("\nType ([L]ight,[M]edium,[R]ich): ");
+    scanf(" %c", &coffeeType1);
+    printf("Bag weight (g): ");
+    scanf("%d", &coffeeWeight1);
+    printf("Best served with cream ([Y]es,[N]o): ");
+    scanf(" %c", &cream1);
+    printf("\nCOFFEE-2...");
+    printf("\nType ([L]ight,[M]edium,[R]ich): ");
+    scanf(" %c", &coffeeType2);
+    printf("Bag weight (g): ");
+    scanf("%d", &coffeeWeight2);
+    printf("Best served with cream ([Y]es,[N]o): ");
+    scanf(" %c", &cream2);
+    printf("\nCOFFEE-3...");
+    printf("\nType ([L]ight,[M]edium,[R]ich): ");
+    scanf(" %c", &coffeeType3);
+    printf("Bag weight (g): ");
+    scanf("%d", &coffeeWeight3);
+    printf("Best served with cream ([Y]es,[N]o): ");
+    scanf(" %c", &cream3);
+    printf("\n");
+    printf("---+------------------------+---------------+-------+");
+    printf("\n   |    Coffee              |   Packaged    | Best  |");
+    printf("\n   |     Type               |  Bag Weight   | Served|");
+    printf("\n   +------------------------+---------------+ With  |");
+    printf("\nID | Light | Medium | Rich  |  (G) | Lbs    | Cream |");
+    printf("\n---+------------------------+---------------+-------|");
+    printf("\n 1 |   %d   |   %d    |   %d   | %4d | %6.3lf |   %d   |",
+        (coffeeType1 == 'L' || coffeeType1 == 'l'),
+        (coffeeType1 == 'M' || coffeeType1 == 'm'),
+        (coffeeType1 == 'R' || coffeeType1 == 'r'),
+        (coffeeWeight1),
+        (coffeeWeight1 / GRAMS_IN_LBS),
+        (cream1 == 'Y' || cream1 == 'y'));
+    printf("\n 2 |   %d   |   %d    |   %d   | %4d | %6.3lf |   %d   |",
+        (coffeeType2 == 'L' || coffeeType2 == 'l'),
+        (coffeeType2 == 'M' || coffeeType2 == 'm'),
+        (coffeeType2 == 'R' || coffeeType2 == 'r'),
+        (coffeeWeight2),
+        (coffeeWeight2 / GRAMS_IN_LBS),
+        (cream1 == 'Y' || cream2 == 'y'));
+    printf("\n 3 |   %d   |   %d    |   %d   | %4d | %6.3lf |   %d   |",
+        (coffeeType3 == 'L' || coffeeType3 == 'l'),
+        (coffeeType3 == 'M' || coffeeType3 == 'm'),
+        (coffeeType3 == 'R' || coffeeType3 == 'r'),
+        (coffeeWeight3),
+        (coffeeWeight3 / GRAMS_IN_LBS),
+        (cream1 == 'Y' || cream3 == 'y'));
+
+    printf("\n\nEnter how you like your coffee...");
+    printf("\n\nCoffee strength ([L]ight, [M]edium, [R]ich): ");
+    scanf(" %c", &coffeeStrength);
+    printf("Do you like your coffee with cream ([Y]es,[N]o): ");
+    scanf(" %c", &likeCream);
+    printf("Typical number of daily servings: ");
+    scanf("%d", &totalServings);
+    printf("\nThe below table shows how your preferences align to the available products:");
+    printf("\n\n--------------------+-------------+-------+");
+    printf("\n  |     Coffee      |  Packaged   | With  |");
+    printf("\nID|      Type       | Bag Weight  | Cream |");
+    printf("\n--+-----------------+-------------+-------+");
+    printf("\n 1|       %d         |      %d      |   %d   |",
+        ((coffeeStrength == 'L' || coffeeStrength == 'l') && (coffeeType1 == 'L' || coffeeType1 == 'l')) || ((coffeeStrength == 'M' || coffeeStrength == 'm') && (coffeeType1 == 'M' || coffeeType1 == 'm')) || ((coffeeStrength == 'R' || coffeeStrength == 'r') && (coffeeType1 == 'R' || coffeeType1 == 'r')),
+        ((totalServings >= 1 && totalServings <= 4) && (coffeeWeight1 >= 250 && coffeeWeight1 < 500)) || ((totalServings >= 5 && totalServings <= 9) && (coffeeWeight1 >= 500 && coffeeWeight1 < 1000)) || ((totalServings >= 10) && (coffeeWeight1 >= 1000)),
+        ((likeCream == 'y' || likeCream == 'Y') && (cream1 == 'Y' || cream1 == 'y')) || ((likeCream == 'n' || likeCream == 'N') && (cream1 == 'n' || cream1 == 'N')));
+    printf("\n 2|       %d         |      %d      |   %d   |",
+        ((coffeeStrength == 'L' || coffeeStrength == 'l') && (coffeeType2 == 'L' || coffeeType2 == 'l')) || ((coffeeStrength == 'M' || coffeeStrength == 'm') && (coffeeType2 == 'M' || coffeeType2 == 'm')) || ((coffeeStrength == 'R' || coffeeStrength == 'r') && (coffeeType2 == 'R' || coffeeType2 == 'r')),
+        ((totalServings >= 1 && totalServings <= 4) && (coffeeWeight2 >= 250 && coffeeWeight2 < 500)) || ((totalServings >= 5 && totalServings <= 9) && (coffeeWeight2 >= 500 && coffeeWeight2 < 1000)) || ((totalServings >= 10) && (coffeeWeight2 >= 1000)),
+        ((likeCream == 'y' || likeCream == 'Y') && (cream2 == 'Y' || cream2 == 'y')) || ((likeCream == 'n' || likeCream == 'N') && (cream2 == 'n' || cream2 == 'N')));
+    printf("\n 3|       %d         |      %d      |   %d   |",
+        ((coffeeStrength == 'L' || coffeeStrength == 'l') && (coffeeType3 == 'L' || coffeeType3 == 'l')) || ((coffeeStrength == 'M' || coffeeStrength == 'm') && (coffeeType3 == 'M' || coffeeType3 == 'm')) || ((coffeeStrength == 'R' || coffeeStrength == 'r') && (coffeeType3 == 'R' || coffeeType3 == 'r')),
+        ((totalServings >= 1 && totalServings <= 4) && (coffeeWeight3 >= 250 && coffeeWeight3 < 500)) || ((totalServings >= 5 && totalServings <= 9) && (coffeeWeight3 >= 500 && coffeeWeight3 < 1000)) || ((totalServings >= 10) && (coffeeWeight3 >= 1000)),
+        ((likeCream == 'y' || likeCream == 'Y') && (cream3 == 'Y' || cream3 == 'y')) || ((likeCream == 'n' || likeCream == 'N') && (cream3 == 'n' || cream3 == 'N')));
+
+    printf("\n\nEnter how you like your coffee...");
+    printf("\n\nCoffee strength ([L]ight, [M]edium, [R]ich): ");
+    scanf(" %c", &coffeeStrength);
+    printf("Do you like your coffee with cream ([Y]es,[N]o): ");
+    scanf(" %c", &likeCream);
+    printf("Typical number of daily servings: ");
+    scanf("%d", &totalServings);
+    printf("\nThe below table shows how your preferences align to the available products:");
+    printf("\n\n--------------------+-------------+-------+");
+    printf("\n  |     Coffee      |  Packaged   | With  |");
+    printf("\nID|      Type       | Bag Weight  | Cream |");
+    printf("\n--+-----------------+-------------+-------+");
+    printf("\n 1|       %d         |      %d      |   %d   |",
+        ((coffeeStrength == 'L' || coffeeStrength == 'l') && (coffeeType1 == 'L' || coffeeType1 == 'l')) || ((coffeeStrength == 'M' || coffeeStrength == 'm') && (coffeeType1 == 'M' || coffeeType1 == 'm')) || ((coffeeStrength == 'R' || coffeeStrength == 'r') && (coffeeType1 == 'R' || coffeeType1 == 'r')),
+        ((totalServings >= 1 && totalServings <= 4) && (coffeeWeight1 >= 250 && coffeeWeight1 < 500)) || ((totalServings >= 5 && totalServings <= 9) && (coffeeWeight1 >= 500 && coffeeWeight1 < 1000)) || ((totalServings >= 10) && (coffeeWeight1 >= 1000)),
+        ((likeCream == 'y' || likeCream == 'Y') && (cream1 == 'Y' || cream1 == 'y')) || ((likeCream == 'n' || likeCream == 'N') && (cream1 == 'n' || cream1 == 'N')));
+    printf("\n 2|       %d         |      %d      |   %d   |",
+        ((coffeeStrength == 'L' || coffeeStrength == 'l') && (coffeeType2 == 'L' || coffeeType2 == 'l')) || ((coffeeStrength == 'M' || coffeeStrength == 'm') && (coffeeType2 == 'M' || coffeeType2 == 'm')) || ((coffeeStrength == 'R' || coffeeStrength == 'r') && (coffeeType2 == 'R' || coffeeType2 == 'r')),
+        ((totalServings >= 1 && totalServings <= 4) && (coffeeWeight2 >= 250 && coffeeWeight2 < 500)) || ((totalServings >= 5 && totalServings <= 9) && (coffeeWeight2 >= 500 && coffeeWeight2 < 1000)) || ((totalServings >= 10) && (coffeeWeight2 >= 1000)),
+        ((likeCream == 'y' || likeCream == 'Y') && (cream2 == 'Y' || cream2 == 'y')) || ((likeCream == 'n' || likeCream == 'N') && (cream2 == 'n' || cream2 == 'N')));
+    printf("\n 3|       %d         |      %d      |   %d   |",
+        ((coffeeStrength == 'L' || coffeeStrength == 'l') && (coffeeType3 == 'L' || coffeeType3 == 'l')) || ((coffeeStrength == 'M' || coffeeStrength == 'm') && (coffeeType3 == 'M' || coffeeType3 == 'm')) || ((coffeeStrength == 'R' || coffeeStrength == 'r') && (coffeeType3 == 'R' || coffeeType3 == 'r')),
+        ((totalServings >= 1 && totalServings <= 4) && (coffeeWeight3 >= 250 && coffeeWeight3 < 500)) || ((totalServings >= 5 && totalServings <= 9) && (coffeeWeight3 >= 500 && coffeeWeight3 < 1000)) || ((totalServings >= 10) && (coffeeWeight3 >= 1000)),
+        ((likeCream == 'y' || likeCream == 'Y') && (cream3 == 'Y' || cream3 == 'y')) || ((likeCream == 'n' || likeCream == 'N') && (cream3 == 'n' || cream3 == 'N')));
+
+
+    printf("\n\nHope you found a product that suits your likes!");
+    printf("\n");
+
+    return 0;
+}
+
